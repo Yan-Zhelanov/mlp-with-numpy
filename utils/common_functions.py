2decfaa86bf560755d18f9ae68feacd4d1b3e58b
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-05 21:44:21 +0300

    Fix: typing, docstrings and refactoring in common functions

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,30 +1,36 @@
-from typing import Union
-import numpy as np
 import random
 
+import numpy as np
 import pandas as pd
 
 
-def read_dataframe_file(path_to_file: str) -> Union[pd.DataFrame, None]:
+def read_dataframe_file(path_to_file: str) -> pd.DataFrame:
     """Reads DataFrame file."""
     if path_to_file.endswith('csv'):
         return pd.read_csv(path_to_file)
-    elif path_to_file.endswith('pickle'):
+    if path_to_file.endswith('pickle'):
         return pd.read_pickle(path_to_file)
-    elif path_to_file.endswith('parquet'):
+    if path_to_file.endswith('parquet'):
         return pd.read_parquet(path_to_file)
-    else:
-        raise ValueError("Unsupported file format")
+    raise ValueError('Unsupported file format!')
 
 
-def set_seed(seed):
+def set_seed(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
 
 
-def convert_lists_and_tuples_to_string(data):
-    """
-    Recursively replaces all lists in a dictionary (including nested dictionaries) with a string representation of the list.
+def convert_lists_and_tuples_to_string(data: dict) -> dict:
+    """Replace recursively all lists and tuples in a dictionary.
+
+    This function replaces all lists and tuples including nested dictionaries
+    with a string representation of the list.
+
+    Args:
+        data (dict): The dictionary to convert.
+
+    Returns:
+        dict: The converted dictionary.
     """
     data = data.copy()
     for key, value in data.items():
@@ -35,4 +41,3 @@ def convert_lists_and_tuples_to_string(data):
         elif value is None:
             data[key] = str(value)
     return data
-
