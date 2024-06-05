66b89c2846c9baf150cd3f6be7649f47bd259219
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-05 19:34:52 +0300

    Update: convert EasyDict to the DataConfig class

--- configs/data_config.py
+++ configs/data_config.py
@@ -1,36 +1,30 @@
 import os
-from easydict import EasyDict
 
 from utils.enums import SamplerType, TransformsType
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-data_config = EasyDict()
 
-# Path to the directory with dataset files
-data_config.path_to_data = os.path.join(ROOT_DIR, 'data', 'emotion_detection')
-data_config.annot_filename = 'data_info.csv'
-
-# Label mapping
-data_config.label_mapping = {
-    'angry': 0,
-    'disgusted': 1,
-    'fearful': 2,
-    'happy': 3,
-    'neutral': 4,
-    'sad': 5,
-    'surprised': 6
-}
-
-data_config.sampler_type = SamplerType.Default
-
-data_config.train_transforms = [
-    (TransformsType.Resize, dict(size=(32, 32))),
-    (TransformsType.ToFloat, dict()),
-    (TransformsType.Normalize, dict(a=-1, b=1)),
-]
-data_config.eval_transforms = [
-    (TransformsType.Resize, dict(size=(32, 32))),
-    (TransformsType.ToFloat, dict()),
-    (TransformsType.Normalize, dict(a=-1, b=1)),
-]
+class DataConfig(object):
+    PATH_TO_DATA = os.path.join(ROOT_DIR, 'data', 'emotion_detection')
+    ANNOTATION_FILE = 'data_info.csv'
+    label_mapping = {
+        'angry': 0,
+        'disgusted': 1,
+        'fearful': 2,
+        'happy': 3,
+        'neutral': 4,
+        'sad': 5,
+        'surprised': 6,
+    }
+    sampler_type = SamplerType.DEFAULT
+    train_transforms = [
+        (TransformsType.RESIZE, dict(size=(32, 32))),
+        (TransformsType.TO_FLOAT, dict()),
+        (TransformsType.NORMALIZE, dict(a=-1, b=1)),
+    ]
+    eval_transforms = [
+        (TransformsType.RESIZE, dict(size=(32, 32))),
+        (TransformsType.TO_FLOAT, dict()),
+        (TransformsType.NORMALIZE, dict(a=-1, b=1)),
+    ]
