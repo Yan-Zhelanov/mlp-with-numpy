7058c945eb92e0b8e3e631d53a248a02a9971ecb
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-05 20:34:25 +0300

    Update: typing and add the TargetTransform abstract class

--- transforms/target_transforms.py
+++ transforms/target_transforms.py
@@ -1,13 +1,28 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 
 
-class OneHotEncoding:
-    """Creates matrix of one-hot encoding vectors for input targets"""
+class TargetTransform(ABC):
+    @abstractmethod
+    def transform(self, targets: np.ndarray) -> np.ndarray:
+        """Transform targets.
 
-    def __init__(self, num_classes: int):
-        self.num_classes = num_classes
+        Args:
+            targets (np.ndarray): The input targets to transform.
 
-    def __call__(self, targets: np.ndarray):
+        Returns:
+            np.ndarray: Transformed targets.
+        """
+
+
+class OneHotEncoding(TargetTransform):
+    """Create matrix of one-hot encoding vectors for input targets."""
+
+    def __init__(self, num_classes: int) -> None:
+        self._num_classes = num_classes
+
+    def transform(self, targets: np.ndarray) -> np.ndarray:
         """
         One-hot encoding vector representation:
             t_i^(k) = 1 if k = t_i otherwise  0,
@@ -18,5 +33,6 @@ class OneHotEncoding:
         Args:
             targets: np.ndarray
         """
-        # TODO: Implement this function, it is possible to do it without loop using numpy
+        # TODO: Implement this function, it is possible to do it without loop
+        # using numpy
         raise NotImplementedError
