18f6840f8a8efee499bbaacf2778ef6638a7cf51
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-10 22:04:17 +0300

    Add: implementation for DataLoader and refactoring

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -1,60 +1,81 @@
+from typing import TypedDict
+
 import numpy as np
+from numpy import typing as npt
 
 from dataloaders import batch_samplers
+from dataset.emotions_dataset import EmotionsDataset
 from utils.enums import SamplerType
 
 
-class Dataloader:
-    """Provides an iterable over the given dataset."""
+class BatchType(TypedDict):
+    image: npt.NDArray[np.integer | np.float_]
+    label: npt.NDArray[np.integer]
 
-    def __init__(self, dataset, batch_size: int, sampler: SamplerType = SamplerType.Default,
-                 shuffle: bool = False, drop_last: bool = False, **kwargs):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.drop_last = drop_last
 
-        self.num_samples = len(self.dataset)
-        self.sampler = getattr(batch_samplers, sampler.name + 'Sampler')(self.dataset, shuffle, **kwargs)
+class DataType(TypedDict):
+    image: npt.NDArray[np.integer | np.float_]
+    label: int
+
+
+class DataLoader(object):
+    """Provide an iterable over the given dataset."""
+
+    def __init__(
+        self,
+        dataset: EmotionsDataset,
+        batch_size: int,
+        sampler: SamplerType = SamplerType.DEFAULT,
+        shuffle: bool = False,
+        drop_last: bool = False,
+        **kwargs,
+    ) -> None:
+        self._dataset = dataset
+        self._batch_size = batch_size
+        self._drop_last = drop_last
+        self._num_samples = len(self._dataset)
+        self._sampler = getattr(
+            batch_samplers, sampler.name + 'Sampler',
+        )(self._dataset, shuffle, **kwargs)
 
     def __iter__(self):
-        """Returns a batch at each iteration."""
-        # TODO:
-        #  1) For every idx in self.sampler:
-        #       - add dataset[idx] to batch_data
-        #       - if length of batch_data is equal to the batch_size:
-        #               - return Dataloader._collate_fn(batch_data) with yield
-        #               - empty the batch_data list
-        #  2) If self.drop_last is False and list of batch_data is not empty, return last batch with yield
+        """Return a batch at each iteration."""
         batch_data = []
-        raise NotImplementedError
+        for index in self._sampler:
+            batch_data.append(self._dataset[index])
+            if len(batch_data) == self._batch_size:
+                yield self._collate_fn(batch_data)
+                batch_data = []
+        if not self._drop_last and len(batch_data) > 0:
+            yield self._collate_fn(batch_data)
 
     @staticmethod
-    def _collate_fn(batch_data: list) -> dict:
-        """Combines a list of samples into a dictionary
+    def _collate_fn(batch: list[DataType]) -> BatchType:
+        """Combine a list of samples into a dictionary
 
         Example:
-            batch_data = [
+            batch = [
                 {'image': img_1, 'label': 0},
                 {'image': img_2, 'label': 1},
                 {'image': img_3, 'label': 1},
                 {'image': img_4, 'label': 2},
             ]
-
             batch = {
                 'image': np.array([img_1, img_2, img_3, img_4]),
                 'label': np.array([0, 1, 1, 2])
             }
 
         Args:
-            batch_data: list of dicts
+            batch_data: list of dicts.
 
         Returns:
-            batch: dict
+            BatchType: dict with labels and images.
         """
-        # TODO:
-        #  1) Get the first item from batch_data to determine which keys each item has
-        #  2) For each key:
-        #       - combine elements from batch_data with that key
-        #       - concatenate data to np.ndarray
-        #  3) Return dict with these keys
-        raise NotImplementedError
+        new_batch: BatchType = {
+            'image': np.array([]),
+            'label': np.array([]),
+        }
+        for data in batch:
+            new_batch['image'] = np.append(new_batch['image'], data['image'])
+            new_batch['label'] = np.append(new_batch['label'], data['label'])
+        return new_batch
