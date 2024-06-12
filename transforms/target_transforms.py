5c00329e44b3efbcd2fc23e2ff73b206568072ff
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-12 19:40:18 +0300

    Add: target transforms implementation and fix docstrings and names

--- transforms/target_transforms.py
+++ transforms/target_transforms.py
@@ -3,19 +3,19 @@ from abc import ABC, abstractmethod
 import numpy as np
 from numpy import typing as npt
 
-TargetsType = npt.NDArray[np.integer | np.floating]
+_TargetsType = npt.NDArray[np.integer | np.floating]
 
 
 class TargetTransform(ABC):
     @abstractmethod
-    def transform(self, targets: TargetsType) -> TargetsType:
+    def transform(self, targets: _TargetsType) -> _TargetsType:
         """Transform targets.
 
         Args:
-            targets (TargetsType): The input targets to transform.
+            targets (_TargetsType): The input targets to transform.
 
         Returns:
-            TargetsType: Transformed targets.
+            _TargetsType: Transformed targets.
         """
 
 
@@ -25,17 +25,18 @@ class OneHotEncoding(TargetTransform):
     def __init__(self, num_classes: int) -> None:
         self._num_classes = num_classes
 
-    def transform(self, targets: TargetsType) -> TargetsType:
-        """
-        One-hot encoding vector representation:
-            t_i^(k) = 1 if k = t_i otherwise  0,
+    def transform(self, targets: _TargetsType) -> _TargetsType:
+        """Transform targets into one-hot encoding vector representation.
 
-            where:
-                - k in [0, self.k-1],
-                - t_i - target class of i-sample.
+        t_i^(k) = 1 if k = t_i otherwise  0,
+        where:
+        - k in [0, self.k-1],
+        - t_i - target class of i-sample.
+
         Args:
-            targets: np.ndarray
+            targets (_TargetsType): The input targets to transform.
+
+        Returns:
+            _TargetsType: Transformed targets.
         """
-        # TODO: Implement this function, it is possible to do it without loop
-        # using numpy
-        raise NotImplementedError
+        return np.eye(self._num_classes)[targets]
