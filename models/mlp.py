43424fdaec9f55d4880ec9b8b832fa5f5f112273
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-13 22:02:14 +0300

    Update: refactoring in MLP

--- models/mlp.py
+++ models/mlp.py
@@ -1,7 +1,10 @@
-import numpy as np
 import sys
 
-from modules.layers.activations import ReLU, Sigmoid, LeakyReLU, Tanh
+import numpy as np
+
+from configs.mlp_config import ModelConfig
+from modules.layers.activations import LeakyReLU, ReLU, Sigmoid, Tanh
+from modules.layers.base import BaseLayer
 from modules.layers.linear import Linear
 from modules.utils.parameter_initialization import ParametersInit
 
@@ -9,25 +12,29 @@ from modules.utils.parameter_initialization import ParametersInit
 class MLP:
     """A class for implementing Multilayer perceptron model."""
 
-    def __init__(self, config):
-        """
+    def __init__(self, config: ModelConfig) -> None:
+        """Initialize MLP model.
+
         Args:
             config: model configurations
         """
-        self.config = config
-        self.params_init = ParametersInit(config.params)
-        self.layers = self._init_layers()
+        self._config = config
+        self._params_init = ParametersInit(config)
+        self._layers = self._init_layers()
 
-    def _init_layers(self):
+    def _init_layers(self) -> list[BaseLayer]:
         """MLP layers initialization.
 
         This method should include the following steps:
-            1. Go through the layers ((layer_name, layer_params) tuples) defined in self.config.layers one by one
-            2. For each layer, get an instance of the layer class with layer_name and layer_params as:
-                layer = getattr(sys.modules[__name__], layer_name)(**layer_params)
-            3. For layer instance call self.params_init function to initialize layer parameters if it has those (!)
-            4. Append the initialized layer to the layers list
-            5. Return list with all the initialized layers
+        1. Go through the layers ((layer_name, layer_params) tuples) defined in
+            self.config.layers one by one.
+        2. For each layer, get an instance of the layer class with layer_name
+            and layer_params as:
+            layer = getattr(sys.modules[__name__], layer_name)(**layer_params)
+        3. For layer instance call self.params_init function to initialize
+            layer parameters if it has those (!).
+        4. Append the initialized layer to the layers list.
+        5. Return list with all the initialized layers.
 
         Returns:
             list of initialized layers
@@ -38,12 +45,12 @@ class MLP:
 
     def train(self):
         """Sets the training mode for each layer."""
-        for layer in self.layers:
+        for layer in self._layers:
             layer.train()
 
     def eval(self):
         """Sets the evaluation mode for each layer."""
-        for layer in self.layers:
+        for layer in self._layers:
             layer.eval()
 
     def __call__(self, inputs: np.ndarray):
@@ -63,13 +70,13 @@ class MLP:
 
     def load_params(self, parameters):
         """Loads model parameters."""
-        assert len(parameters) == len(self.layers)
-        for i, layer in enumerate(self.layers):
+        assert len(parameters) == len(self._layers)
+        for i, layer in enumerate(self._layers):
             layer.load_params(parameters[i])
 
     def get_params(self):
         """Returns model parameters."""
         parameters = []
-        for layer in self.layers:
+        for layer in self._layers:
             parameters.append(layer.get_params())
         return parameters
