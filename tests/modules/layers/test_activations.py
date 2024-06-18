7f163a079871cf91627376d025df79a544e2b210
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-18 20:46:39 +0300

    Add: test for LeakyReLU

--- tests/modules/layers/test_activations.py
+++ tests/modules/layers/test_activations.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from modules.layers.activations import ReLU
+from modules.layers.activations import LeakyReLU, ReLU
 
 
 def test_relu():
@@ -19,3 +19,12 @@ def test_relu_compute_gradient():
     result = relu.compute_backward_gradient(np.array([[2, 3, 4]]))
 
     assert (result == np.array([[0, 3, 0]])).all()
+
+
+def test_leaky_relu():
+    leaky_relu = LeakyReLU(0.1)
+
+    result = leaky_relu(np.array([[0, 1, -1]]))
+
+    assert (result == np.array([0, 1, -0.1])).all()
+    assert (leaky_relu._inputs_cache == np.array([[0, 1, -1]])).all()
