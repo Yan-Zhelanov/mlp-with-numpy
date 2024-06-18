94e8ee45582ea4f656816dac05f24ff71182071d
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-18 20:39:13 +0300

    Add: test for ReLU gradient

--- tests/modules/layers/test_activations.py
+++ tests/modules/layers/test_activations.py
@@ -10,3 +10,12 @@ def test_relu():
 
     assert (result == np.array([0, 1, 0])).all()
     assert (relu._inputs_cache == np.array([[0, 1, -1]])).all()
+
+
+def test_relu_compute_gradient():
+    relu = ReLU()
+    relu._inputs_cache = np.array([[0, 1, -1]])
+
+    result = relu.compute_backward_gradient(np.array([[2, 3, 4]]))
+
+    assert (result == np.array([[0, 3, 0]])).all()
