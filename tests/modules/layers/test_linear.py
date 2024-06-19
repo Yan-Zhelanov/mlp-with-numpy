5babe82717786ca8d3729eb3f552ed184f605596
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-19 22:36:51 +0300

    Add: test for the Linear gradient

--- tests/modules/layers/test_linear.py
+++ tests/modules/layers/test_linear.py
@@ -13,3 +13,20 @@ def test_linear():
 
     assert (result == expected_result).all()
     assert (linear._inputs_cache == expected_cache).all()
+
+
+def test_linear_compute_gradient():
+    linear = Linear(2, 3)
+    linear._weights = np.array([[1, 2], [2, 3], [3, 4]])
+    linear._inputs_cache = np.array([[1, 2], [3, 4]])
+    expected_result = np.array([[38, 56], [56, 83]])
+    expected_weights_gradient = np.array([[29, 42], [33, 48], [37, 54]])
+    expected_bias_gradient = np.array([13, 15, 17])
+
+    result = linear.compute_backward_gradient(
+        np.array([[5, 6, 7], [8, 9, 10]]),
+    )
+
+    assert (result == expected_result).all()
+    assert (linear._weights_gradient == expected_weights_gradient).all()
+    assert (linear._bias_gradient == expected_bias_gradient).all()
