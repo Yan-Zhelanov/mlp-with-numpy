2379d47163d5daa91b4a13913cd950262762d72e
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-20 21:19:18 +0300

    Add: implementation for the softmax function

--- modules/layers/softmax.py
+++ modules/layers/softmax.py
@@ -1,21 +1,30 @@
 import numpy as np
+from numpy import typing as npt
 
 
-def softmax(logits: np.ndarray):
-    """Softmax function.
+def calculate_softmax(
+    logits: npt.NDArray[np.floating],
+) -> npt.NDArray[np.floating]:
+    """Calculate softmax function.
 
     The formula for numerically stable softmax function is:
+    y_j = e^(z_j - c) / Σ(i=0 to K-1) e^(z_i - c)
 
-        y_j = e^(z_j - c) / Σ(i=0 to K-1) e^(z_i - c)
+    where:
+    - y_j is the softmax probability of class j,
+    - z_j is the model output (logits) for class j before softmax,
+    - K is the total number of classes,
+    - c is maximum(z),
+    - Σ denotes summation.
 
-        where:
-            - y_j is the softmax probability of class j,
-            - z_j is the model output (logits) for class j before softmax,
-            - K is the total number of classes,
-            - c is maximum(z),
-            - Σ denotes summation.
+    Args:
+        logits (npt.NDArray[np.floating]): The model output before softmax.
+
     Returns:
-        np.ndarray: the softmax probabilities
+        npt.NDArray[np.floating]: The softmax probabilities.
     """
-    # TODO: Implement numerically stable softmax function
-    raise NotImplementedError
+    max_logits = np.max(logits, axis=1, keepdims=True)
+    logits -= max_logits
+    numerator = np.exp(logits)
+    denominator = np.sum(numerator, axis=1, keepdims=True)
+    return numerator / denominator
