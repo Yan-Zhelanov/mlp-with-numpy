c950f526324d35940fd6c9e8ec8f95501ef70921
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-23 18:30:44 +0300

    Add: test for CrossEntropyLoss gradient

--- tests/modules/test_cross_entropy_loss.py
+++ tests/modules/test_cross_entropy_loss.py
@@ -43,3 +43,28 @@ def test_cross_entropy_loss_numerical_stability():
     computed_loss = loss(targets, logits)
 
     assert np.isclose(computed_loss, expected_loss, rtol=_TOLERANCE)
+
+
+def test_cross_entropy_loss_gradient():
+    targets = np.array(
+        [
+            [1, 0, 0], [0, 1, 0], [0, 0, 1],
+        ], dtype=np.float32,
+    )
+    logits = np.array(
+        [
+            [2, 2, -1], [-1, 2, 2], [2, -1, 2],
+        ], dtype=np.float32,
+    )
+    expected_gradient = np.array(
+        [
+            [-0.17071481, 0.16261852, 0.0080963],
+            [0.0080963, -0.17071481, 0.16261852],
+            [0.16261852, 0.0080963, -0.17071481],
+        ], dtype=np.float32,
+    )
+
+    loss = CrossEntropyLoss()
+    computed_gradient = loss.compute_backward_gradient(targets, logits)
+
+    assert np.allclose(computed_gradient, expected_gradient, rtol=_TOLERANCE)
