e4b704f8969207597d6534c658e7ece55e866f7a
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-24 21:42:15 +0300

    Add: test for the __call__ method in MLP

--- tests/models/test_mlp.py
+++ tests/models/test_mlp.py
@@ -1,11 +1,21 @@
+import numpy as np
+
 from configs.mlp_config import WeightsInitType
 from models.mlp import MLP
 from modules.layers.linear import Linear
 
+_INPUT_SHAPE = 4
+_OUTPUT_SHAPE = 2
+_TOLERANCE = 0.1
+
 
 class MockConfig:
     LAYERS = [
-        ('Linear', {'input_shape': 1024, 'output_shape': 128}),
+        (
+            'Linear', {
+                'input_shape': _INPUT_SHAPE, 'output_shape': _OUTPUT_SHAPE,
+            },
+        ),
     ]
     INIT_TYPE = WeightsInitType.NORMAL
     INIT_KWARGS = {'mu': 0, 'sigma': 0.001}
@@ -20,5 +30,18 @@ def test_init_layers():
 
     assert len(layers) == 1
     assert isinstance(layers[0], Linear)
-    assert layers[0]._input_shape == 1024
-    assert layers[0]._output_shape == 128
+    assert layers[0]._input_shape == _INPUT_SHAPE
+    assert layers[0]._output_shape == _OUTPUT_SHAPE
+
+
+def test_mlp():
+    config = MockConfig()
+    mlp = MLP(config)
+    inputs = np.array([1, 2, 3, 4]).reshape((1, 4))
+
+    inputs = mlp(inputs)
+
+    assert (inputs[0] != 0).all()
+    assert np.allclose(
+        inputs, np.array([0, 0]).reshape((1, 2)), atol=_TOLERANCE,
+    )
