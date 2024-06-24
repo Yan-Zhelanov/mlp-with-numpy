adc748af95292302d220af8bb9515f884e1d8f10
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-24 20:06:51 +0300

    Update: names, docstrings, and typing in parameter initialization

--- modules/utils/parameter_initialization.py
+++ modules/utils/parameter_initialization.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import typing as npt
 
 from configs.mlp_config import ModelConfig
 
@@ -7,89 +8,131 @@ class ParametersInit:
     """Training parameters initialization."""
 
     def __init__(self, config: ModelConfig):
-        self.config = config
-
-        # Initialization function according to init_type
-        self.init_function = getattr(self, self.config.init_type.name)
+        self._config = config
+        self._init_function = getattr(
+            self, f'get_init_{self._config.INIT_TYPE.name.lower()}',
+        )
 
     def __call__(self, layer):
         """Initializing layer parameters"""
         for param_name in layer.parameters:
             param = getattr(layer, param_name)
-
-            if param_name == 'bias' and self.config.zero_bias:
-                setattr(layer, param_name, self.zeros(param.shape))
+            if param_name == 'bias' and self._config.ZERO_BIAS:
+                setattr(layer, param_name, self.get_init_zeros(param.shape))
             else:
-                setattr(layer, param_name, self.init_function(param.shape))
+                setattr(layer, param_name, self._init_function(param.shape))
 
-    def zeros(self, param_shape):
-        """Initialization with zeros."""
+    def get_init_zeros(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
+        """Initialize with zeros.
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
+        """
         return np.zeros(param_shape)
 
-    def normal(self, param_shape):
+    def get_init_normal(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
         """Initialization with values from a normal distribution.
 
         W ~ N(mu, sigma^2),
-
         where:
-            - mu and sigma can be defined in self.config.init_kwargs
+        - mu and sigma can be defined in self.config.init_kwargs
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
         """
         # TODO: Implement this method using np.random.normal
         #  (method should return initialized values)
         raise NotImplementedError
 
-    def uniform(self, param_shape):
-        """Initialization with values from a uniform distribution.
+    def get_init_uniform(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
+        """Initialize with values from a uniform distribution.
 
         W ~ U(-epsilon, epsilon),
 
         where:
-            - epsilon can be defined in self.config.init_kwargs
+        - epsilon can be defined in self.config.init_kwargs
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
         """
         # TODO: Implement this method using np.random.uniform
         #  (method should return initialized values)
         raise NotImplementedError
 
-    def he(self, param_shape):
-        """He initialization.
+    def get_init_he(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
+        """Initialize He.
 
-        Initialization with values from a normal distribution with the following parameters:
-
-        W ~ N(0, 2 / M_{l-1}),
-
+        Initialization with values from a normal distribution with the
+        following parameters: W ~ N(0, 2 / M_{l-1}),
         where:
-            - M_{l-1} - number of input features of a layer
+        - M_{l-1} - number of input features of a layer
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
         """
         # TODO: Implement this method using np.random.normal
         #  (method should return initialized values)
         raise NotImplementedError
 
-    def xavier(self, param_shape):
+    def get_init_xavier(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
         """Xavier initialization.
 
-        Initialization with values from a uniform distribution with the following parameters:
-
-        W ~ U(-epsilon, epsilon),
+        Initialization with values from a uniform distribution with the
+        following parameters: W ~ U(-epsilon, epsilon),
 
         where:
-            - epsilon = \sqrt{1 / M_{l-1}},
-            - M_{l-1} - number of input features of a layer
+        - epsilon = sqrt{1 / M_{l-1}},
+        - M_{l-1} - number of input features of a layer
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
         """
         # TODO: Implement this method using np.random.uniform
         #  (method should return initialized values)
         raise NotImplementedError
 
-    def xavier_normalized(self, param_shape):
+    def get_init_xavier_normalized(
+        self, param_shape: tuple[int, ...],
+    ) -> npt.NDArray[np.floating]:
         """Xavier normalized initialization.
 
-        Initialization with values from a uniform distribution with the following parameters:
-
-        W ~ U(-epsilon, epsilon),
-
+        Initialization with values from a uniform distribution with the
+        following parameters: W ~ U(-epsilon, epsilon),
         where:
-            - epsilon = \sqrt{6 / (M_{l-1} + M_l)},
-            - M_{l-1} - number of input features of a layer,
-            - M_l - number of output features of a layer.
+        - epsilon = sqrt{6 / (M_{l-1} + M_l)},
+        - M_{l-1} - number of input features of a layer,
+        - M_l - number of output features of a layer.
+
+        Args:
+            param_shape: shape of the parameters.
+
+        Returns:
+            npt.NDArray[np.floating]: initialized parameters.
         """
         # TODO: Implement this method using np.random.uniform
         #  (method should return initialized values)
