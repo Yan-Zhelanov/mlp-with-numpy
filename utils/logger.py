2acf57519b753df5245ba94c6752ff740de3b20e
Yan-Zhelanov <yzhelanov@gmail.com>
2024-06-26 20:23:19 +0300

    Update: constant names, typing, and refactoring in loggers module

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,12 @@
 import os
 from abc import ABC, abstractmethod
-from typing import Union, List
+from typing import List, Union
 
 import mlflow
 import neptune
 from dotenv import load_dotenv
 
+from configs.experiment_config import ExperimentConfig
 from utils.common_functions import convert_lists_and_tuples_to_string
 
 
@@ -37,72 +38,89 @@ class BaseLogger(ABC):
 class NeptuneLogger(BaseLogger):
     """A neptune.ai experiment logger class."""
 
-    def __init__(self, config):
-        super().__init__(config)
-        load_dotenv(config.env_path)
-
+    def __init__(self, config: ExperimentConfig) -> None:
+        load_dotenv(config.NEPTUNE_ENV_PATH)
         self.run = neptune.init_run(
-            project=config.project,
+            project=config.NEPTUNE_PROJECT,
             api_token=os.environ['NEPTUNE_API_TOKEN'],
-            name=config.experiment_name,
-            dependencies=config.dependencies_path,
-            with_id=config.run_id
+            name=config.NEPTUNE_EXPERIMENT_NAME,
+            dependencies=config.NEPTUNE_DEPENDENCIES_PATH,
+            with_id=config.NEPTUNE_RUN_ID,
         )
 
     def log_hyperparameters(self, params: dict):
-        """Model hyperparameters logging."""
-        self.run[f'hyperparameters'] = convert_lists_and_tuples_to_string(params)
+        """Log model hyperparameters logging."""
+        self.run['hyperparameters'] = convert_lists_and_tuples_to_string(
+            params,
+        )
 
-    def save_metrics(self, type_set, metric_name: Union[List[str], str], metric_value: Union[List[float], float],
-                     step=None):
-        if isinstance(metric_name, List):
+    def save_metrics(
+        self,
+        type_set,
+        metric_name: Union[List[str], str],
+        metric_value: Union[List[float], float],
+        step=None,
+    ) -> None:
+        if isinstance(metric_name, list) ^ isinstance(metric_value, list):
+            raise ValueError(
+                'metric_name and metric_value must be of the same type!',
+            )
+        if isinstance(metric_name, list) and isinstance(metric_value, list):
             for p_n, p_v in zip(metric_name, metric_value):
-                self.run[f"{type_set}/{p_n}"].append(p_v)
+                self.run[f'{type_set}/{p_n}'].append(p_v)
         else:
-            self.run[f"{type_set}/{metric_name}"].append(metric_value)
+            self.run[f'{type_set}/{metric_name}'].append(metric_value)
 
     def save_plot(self, type_set, plot_name, plt_fig):
-        self.run[f"{type_set}/{plot_name}"].append(plt_fig)
+        self.run[f'{type_set}/{plot_name}'].append(plt_fig)
 
     def stop(self):
         self.run.stop()
 
 
 class MLFlowLogger(BaseLogger):
-    def __init__(self, config):
-        super().__init__(config)
-        if config.tracking_uri:
-            mlflow.set_tracking_uri(config.tracking_uri)
-
+    def __init__(self, config: ExperimentConfig) -> None:
+        if config.MLFLOW_TRACKING_URI:
+            mlflow.set_tracking_uri(config.MLFLOW_TRACKING_URI)
         self._init_experiment(config)
 
-    def _init_experiment(self, config):
+    def _init_experiment(self, config: ExperimentConfig):
         """Set up experiment configurations to log."""
-        mlflow.set_experiment(config.experiment_name or "default_experiment")
-
-        if config.run_id:
-            mlflow.start_run(run_id=config.run_id)
+        mlflow.set_experiment(
+            config.MLFLOW_EXPERIMENT_NAME or 'default_experiment',
+        )
+        if config.MLFLOW_RUN_ID:
+            mlflow.start_run(run_id=config.MLFLOW_RUN_ID)
         else:
             mlflow.start_run()
-
-        mlflow.log_artifact(config.dataset_version)
-        mlflow.log_artifact(config.dataset_preprocessing)
-        mlflow.log_artifact(config.dependencies_path)
+        mlflow.log_artifact(config.MLFLOW_DATASET_VERSION)
+        mlflow.log_artifact(config.MLFLOW_DATASET_PREPROCESSING)
+        mlflow.log_artifact(config.MLFLOW_DEPENDENCIES_PATH)
 
     def log_hyperparameters(self, params: dict):
         mlflow.log_params(params)
 
-    def save_metrics(self, type_set, metric_name: Union[List[str], str], metric_value: Union[List[float], float], step):
-        if isinstance(metric_name, List):
+    def save_metrics(
+        self,
+        type_set,
+        metric_name: Union[List[str], str],
+        metric_value: Union[List[float], float],
+        step,
+    ) -> None:
+        if isinstance(metric_name, list) ^ isinstance(metric_value, list):
+            raise ValueError(
+                'metric_name and metric_value must be of the same type!',
+            )
+        if isinstance(metric_name, list) and isinstance(metric_value, list):
             for p_n, p_v in zip(metric_name, metric_value):
-                mlflow.log_metric(f"{type_set}_{p_n}", p_v, step)
+                mlflow.log_metric(f'{type_set}_{p_n}', p_v, step)
         else:
-            mlflow.log_metric(f"{type_set}_{metric_name}", metric_value, step)
+            mlflow.log_metric(f'{type_set}_{metric_name}', metric_value, step)
 
     def save_plot(self, type_set, plot_name, plt_fig):
-        plot_path = f"temp_plots/{type_set}_{plot_name}.png"
+        plot_path = f'temp_plots/{type_set}_{plot_name}.png'
         plt_fig.savefig(plot_path)
-        mlflow.log_artifact(plot_path, artifact_path="plots")
+        mlflow.log_artifact(plot_path, artifact_path='plots')
 
     def stop(self):
         mlflow.end_run()
